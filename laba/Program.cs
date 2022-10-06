46583d3e46ecc77242ac525c978bf4b75390eee4
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-07 02:29:07 +0800

    update

--- laba/Program.cs
+++ laba/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace laba
 {
+    public class Logic
+    {
+        public static string SwapChar(string word)
+        {
+
+            StringBuilder word2 = new StringBuilder(word);
+
+            char charWord;
+            for (int i = 0; i < word.Length; i += 2)
+            {
+                charWord = word2[i];
+                word2[i] = word2[i + 1];
+                word2[i + 1] = charWord;
+            }
+
+            var message = "Итоговое слово: " + word2;
+
+            return message;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            string word = "ИРНИТУ";
+            Console.WriteLine("Начально слово: " + word);
+            var message = Logic.SwapChar(word);
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
---
+++ labaTests/LogicTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using laba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        
+
+        [TestMethod()]
+        public void SwapCharTest()
+        {
+            var message = Logic.SwapChar("ИРНИТУ");
+            Assert.AreEqual("Итоговое слово: РИИНУТ", message);
+        }
+    }
+}
